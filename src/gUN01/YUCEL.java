5bef7303ede4d36299c07157948181ddee997078
muge <mugeurusan@yahoo.com.tr>
2022-07-10 11:54:12 +0300

    degisikliller

--- src/gUN01/YUCEL.java
+++ src/gUN01/YUCEL.java
@@ -4,5 +4,7 @@ public class YUCEL {
     public static void main(String[] args) {
         System.out.println("deniz kızım");
         System.out.println("yucel yazıldı");
+        System.out.println("muge geldi");
+        System.out.println("tatil oldu");
     }
 }
